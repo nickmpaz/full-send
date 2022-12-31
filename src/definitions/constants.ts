2b997766ff795e9d8d69d0102c74bf0370da9a92
Nick Paz <nicholasmpaz@gmail.com>
2022-12-31 09:41:23 -0800

    rebuild

--- src/definitions/constants.ts
+++ src/definitions/constants.ts
@@ -1,5 +1,5 @@
 const commandLineName = "full-send";
 const commandLineDescription = "description";
-const commandLineVersion = "0.0.1";
+const commandLineVersion = "0.0.2";
 
 export { commandLineName, commandLineDescription, commandLineVersion };
