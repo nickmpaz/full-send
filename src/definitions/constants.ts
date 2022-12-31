79369f37d8bbe301a6dc11c7e5f67ff24e0e2185
Nick Paz <nicholasmpaz@gmail.com>
2022-12-31 12:31:38 -0800

    update cli version

--- src/definitions/constants.ts
+++ src/definitions/constants.ts
@@ -1,5 +1,5 @@
 const commandLineName = "full-send";
 const commandLineDescription = "description";
-const commandLineVersion = "0.0.2";
+const commandLineVersion = "0.0.3";
 
 export { commandLineName, commandLineDescription, commandLineVersion };
